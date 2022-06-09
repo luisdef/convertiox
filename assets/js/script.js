4bb7d1391e0e9e925fda37c36c05a3882d63e29d
luisdef <luis.assmann.1234@gmail.com>
2022-06-09 20:39:02 -0300

    Conexão com a CoinAPI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // Change theme
 const body = document.querySelector('body');
+
 const changeThemeButton = document.getElementById('lamp');
 
 function toggleTheme() {
@@ -14,8 +15,93 @@ if (localStorage["bodyTheme"] == "dark") {
     toggleTheme();
 } else {
     localStorage["bodyTheme"] = body.className;
+
     localStorage["buttonTheme"] = changeThemeButton.className;
 }
+
 changeThemeButton.onclick = function() {
     toggleTheme();
 }
+
+
+
+// conversion example
+// https://rest.coinapi.io/v1/exchangerate/ETH/BRL?apikey=????
+
+const baseUrlCoinApi = "https://rest.coinapi.io";
+const allImagesCoinApi = "/v1/assets/icons/18?apikey=";
+const coinApiKey = "31C67320-5F39-499A-87E3-BEFCB3272316";
+
+fetch("./assets/js/support.json")
+    .then(res => {
+        return res.json();
+    }).then(data => {
+        const support = data["supportedCurrencies"];
+        
+        const selDe = document.querySelector("#de");
+        const selPara = document.querySelector("#para");
+
+        support.forEach((obj) => {
+            let optDe = document.createElement("option");
+            optDe.value = obj["id"];
+            optDe.setAttribute("id", obj["id"]);
+            optDe.innerText = obj["id"] + " - " + obj["name"];
+            
+            selDe.appendChild(optDe);
+
+            let optPara = document.createElement("option");
+            optPara.value = obj["id"];
+            optPara.setAttribute("id", obj["id"]+"para");
+            optPara.innerText = obj["id"] + " - " + obj["name"];
+            
+            selPara.appendChild(optPara);
+        })
+
+    })
+.catch(err => {
+    console.info(err);
+});
+
+
+const buttonConvert = document.querySelector("button#converter");
+
+
+buttonConvert.addEventListener('click', (e) => {
+    let de = document.querySelector("#de").value;
+
+    let para = document.querySelector("#para").value;
+    
+    let quantia = document.querySelector("#quantia").value;
+    
+    if (de == "" || para == "" || quantia == "")
+    {
+        alert("Preencha corretamente todos os campos antes de converter as moedas.");
+        return false;
+    }
+    else
+    {
+        fetch(baseUrlCoinApi+`/v1/exchangerate/${de}/${para}?apikey=${coinApiKey}`)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                
+                let valorUnit = data["rate"];
+
+                let valorConvertido = valorUnit * quantia;
+
+                let elementoValor = document.querySelector(".valor");
+
+                let valorFormatado = new Intl.NumberFormat(
+                    'pt-BR',
+                    { maximumSignificantDigits: 3 }
+                ).format(valorConvertido);
+
+                elementoValor.innerHTML = valorFormatado;
+                
+            })
+        .catch(err => {
+            console.info(err);
+        });
+    }
+});
